4bf2d063b7eab041a9809920f96c120c669d9eab
Jonas Rafael <jonasrafael97@hotmail.com>
2023-02-09 04:08:32 -0300

    Add Hello World endpoint

---
+++ src/BookLibrary/WebApi/Controllers/v1/HelloWorld/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers.Base;
+
+namespace WebApi.Controllers.v1.HelloWorld;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloWorldController : ValidatorControllerBase
+{
+    [HttpGet]
+    public IActionResult GetHelloWorld()
+    {
+        return Ok("Hello World!");
+    }
+}
