5a99699999c00d6897253ca75b56174bd06ba85f
Jonas Rafael <jonasrafael97@hotmail.com>
2023-02-09 09:12:20 -0300

    Add Pagination object

---
+++ src/BookLibrary/Domain/DTOs/PaginationDto.cs
@@ -0,0 +1,41 @@
+﻿namespace Domain.DTOs;
+
+public class PaginationDto<T>
+{
+    public int TotalOfItems { get; }
+    public const int ItemsPerPage = 10;
+    public int CurrentPage { get; }
+    public int TotalPages { get; private set; }
+    public List<T> Items { get; private set; }
+
+    public PaginationDto(List<T> items, int page)
+    {
+        TotalOfItems = items.Count;
+        SetTotalPages();
+        CurrentPage = page;
+        CheckOutOfRange();
+        SetItems(items);
+    }
+
+    private void CheckOutOfRange()
+    {
+        if (CurrentPage < 1 || CurrentPage > TotalPages) throw new IndexOutOfRangeException();
+    }
+
+    private void SetTotalPages()
+    {
+        TotalPages = (int)Math.Ceiling((double)TotalOfItems/ItemsPerPage);
+        if (TotalPages == 0) TotalPages = 1;
+    }
+
+    private void SetItems(List<T> items)
+    {
+        if (TotalOfItems == 0) Items = new List<T>();
+        else
+        {
+            var begin = (CurrentPage - 1) * ItemsPerPage;
+            var end = CurrentPage * ItemsPerPage - 1 > TotalOfItems ? TotalOfItems - 1 : CurrentPage * ItemsPerPage - 1;
+            Items = items.GetRange(begin, end); 
+        }
+    }
+}
